bb8968543aafc65c3200db27580e3162bc98ee5f
fracabu <100199965+fracabu@users.noreply.github.com>
2022-06-09 15:09:39 +0200

    vue

---
+++ script.js
@@ -0,0 +1,14 @@
+const appVue = new Vue({
+	// selettore dell'elemento html dove avviare vue
+    el: "#app",
+
+  // Conterrà tutti i dati necessari all'applicazione
+    data: {
+	  // varie chiavi necessarie all'app
+    },
+	
+	// Conterrà una serie di funzioni
+	methods: {
+		// funzioni che useremo nell'app
+	}
+});
